86fe73d71051acab877eaf2ead79bf26829934bc
patrickpie <patrickpie830907@gmail.com>
2023-11-28 10:59:27 +0800

    week6修正修正

--- all.js
+++ all.js
@@ -1,36 +1,3 @@
-let data = [
-    {
-      "id": 0,
-      "name": "肥宅心碎賞櫻3日",
-      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-      "area": "高雄",
-      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-      "group": 87,
-      "price": 1400,
-      "rate": 10
-    },
-    {
-      "id": 1,
-      "name": "貓空纜車雙程票",
-      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台北",
-      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-      "group": 99,
-      "price": 240,
-      "rate": 2
-    },
-    {
-      "id": 2,
-      "name": "台中谷關溫泉會1日",
-      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台中",
-      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-      "group": 20,
-      "price": 1765,
-      "rate": 7
-    }
-  ];
-
 const ticketCardArea=document.querySelector(".ticketCard-area");
 const regionSearch=document.querySelector(".regionSearch");
 const searchResultText=document.querySelector("#searchResult-text");
@@ -52,28 +19,40 @@ const ticketNumMessage=document.querySelector("#ticketNum-message");
 const ticketRateMessage=document.querySelector("#ticketRate-message");
 const ticketDescriptionMessage=document.querySelector("#ticketDescription-message");
 
+
+const url="https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+let data = [];
+
+
 //修正，新增完套票後表單輸入框應清除
 const addTicketForm=document.querySelector(".addTicket-form");
 
 //修正，新增查無關鍵字區呈現
 const cantFindArea=document.querySelector(".cantFind-area");
 
+const regionSearchSelectReset=`<option value="地區搜尋" disabled selected hidden>地區搜尋</option>
+<option value="全部地區">全部地區</option>
+<option value="台北">台北</option>
+<option value="台中">台中</option>
+<option value="台南">台南</option>
+<option value="高雄">高雄</option>`;
 
 //初始化函式
 function init(){
+    //取得資料
+    axios.get(url)
+    .then(function(res){
+        data=res.data.data;
+        
+        render(data);
+        //將渲染函式包在then裡面、賦予data之後，確保取得data後才會執行渲染
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 
-    // 修正，新增完套票後表單輸入框應清除
-    addTicketForm.reset();
+    regionSearch.innerHTML=regionSearchSelectReset;
 
-    render(data);
-
-    //修正，新增套票後搜尋下拉選單應恢復為初始狀態
-    regionSearch.innerHTML=`<option value="地區搜尋" disabled selected hidden>地區搜尋</option>
-    <option value="全部地區">全部地區</option>
-    <option value="台北">台北</option>
-    <option value="台中">台中</option>
-    <option value="台南">台南</option>
-    <option value="高雄">高雄</option>`;
 }
 
 init();
@@ -229,8 +208,12 @@ addTicketBtn.addEventListener("click",function(){
             //修正，需賦予id值，此為隨機碼
             "id":myuuid
         })
+        // 修正，新增完套票後表單輸入框應清除
+        addTicketForm.reset();
+        //修正，新增套票後搜尋下拉選單應恢復為初始狀態
+        regionSearch.innerHTML=regionSearchSelectReset;
 
-        init();
+        render(data);
     }
 
 })
